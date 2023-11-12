811a1eb5f3016f68f2d34cd746d23d144aedc0b2
BrendaSilveira04 <116688923+BrendaSilveira04@users.noreply.github.com>
2023-11-11 23:40:13 -0300

    Att Home

--- home.php
+++ home.php
@@ -9,16 +9,26 @@
 <body>
     <div>
         <div class="logoLelo">
-            <img src="imagens/ImgSemFundo.png">
+            <img src="imagens/ImagemMaiorAtt.png">
         </div>
         <div class="centerHome">
-            <div type="submit" id="ButonHome">Histórico</div>
+            <div id="ButonHome">
+                <img src="imagens/Historico.svg" style="width: 79px; height: 79px; margin-top: 15%;">
+                <p>Histórico</p>
+            </div>
 
-            <div type="submit" id="ButonHome">Caixa</div>
-
-            <div type="submit" id="ButonHome">Estoque</div>
-
-            <div type="submit" id="ButonHome">Produtos</div>
+            <div id="ButonHome">
+                <img src="imagens/Caixa.svg" style="width: 81px; height: 81px; margin-top: 15%;">
+                <p>Caixa</p>
+               </div>
+            <div id="ButonHome">
+                <img src="imagens/Estoque.svg" style="width: 81px; height: 81px; margin-top: 15%;">
+                <p>Estoque</p>
+               </div> 
+            <div id="ButonHome">
+                <img src="imagens/Produtos.svg" style="width: 81px; height: 81px; margin-top: 15%;">
+                <p>Produtos</p>
+               </div>
         </div>
     </div>
 </body>
